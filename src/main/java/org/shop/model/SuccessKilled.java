e7859592a9dd94b71288a00c229a432e4ca55240
yezhwi <yzhw.2008@163.com>
2016-11-04 11:14:25 +0800

    model

---
+++ src/main/java/org/shop/model/SuccessKilled.java
@@ -0,0 +1,74 @@
+package org.shop.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Yezhiwei on 16/11/4.
+ */
+public class SuccessKilled implements Serializable{
+
+    private static final long serialVersionUID = -3169396241057792748L;
+
+    private long seckillId;
+
+    private long userPhone;
+
+    private short state;
+
+    private Date createTime;
+
+    // 多对一
+    private Seckill seckill;
+
+    public long getSeckillId() {
+        return seckillId;
+    }
+
+    public void setSeckillId(long seckillId) {
+        this.seckillId = seckillId;
+    }
+
+    public long getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(long userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public short getState() {
+        return state;
+    }
+
+    public void setState(short state) {
+        this.state = state;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Seckill getSeckill() {
+        return seckill;
+    }
+
+    public void setSeckill(Seckill seckill) {
+        this.seckill = seckill;
+    }
+
+    @Override
+    public String toString() {
+        return "SuccessKilled{" +
+                "seckillId=" + seckillId +
+                ", userPhone=" + userPhone +
+                ", state=" + state +
+                ", createTime=" + createTime +
+                ", seckill=" + seckill +
+                '}';
+    }
+}
